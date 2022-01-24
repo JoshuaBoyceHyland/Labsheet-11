93058e5de38441f227521cee38227e5c0d4405f0
c00270917 <c00270917@A307C012>
2022-01-24 09:31:15 +0000

    fixed Q3

--- Q3/Q3/Source.cpp
+++ Q3/Q3/Source.cpp
@@ -10,33 +10,39 @@
 
 #include <iostream>
 
-void findLowest(int&t_lowest, const int t_arraySize,int t_array[]); 
+int findLowest(const int t_arraySize,int t_array[]); 
 
 int main()
 {	
 	const int ARRAY_SIZE = 5; 
 	int lowest = 0; 
-	int array[ARRAY_SIZE]={ 5, 3, 6, 4, 2};
+	int array[ARRAY_SIZE]={ 5, 3, 1, 4, 2};
 
-	findLowest(lowest, ARRAY_SIZE, array); 
+	lowest =  findLowest(ARRAY_SIZE, array); 
 
 	// result
 	std::cout << "The lowest number in the array is: " <<lowest << std::endl; 
 	return 1; 
 }
 
-void findLowest(int&t_lowest, const int t_arraySize, int t_array[])// finds lowest number in array
+int findLowest(const int t_arraySize, int t_array[])// finds lowest number in array
 {
+	int lowest = 0; 
+
 	for (int index = 0; index < t_arraySize; index++)
 	{
+		; 
+
 		if (index == 0)// assign for comparision
 		{
-			t_lowest = t_array[index]; 
+			lowest = t_array[index]; 
 		}
 		
-		if (t_lowest > t_array[index])// finds lowest
+		if (lowest > t_array[index])// finds lowest
 		{
-			t_lowest = t_array[index];
+			lowest = t_array[index];
 		}
 	}
+	
+return lowest; 
 }
